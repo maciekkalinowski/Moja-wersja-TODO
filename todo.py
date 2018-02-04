194dad38d95804111a7f8b8ed8b1426c2d8d02f0
Maciek Kalinowski <maciek.kalinowski@gmail.com>
2018-02-04 19:11:32 +0100

    Usunąłem z funkcji inputy

--- todo.py
+++ todo.py
@@ -1,10 +1,13 @@
-def dodaj(a):
-    nowe_zadanie = input('Wpisz nowe zadanie: \n')
+def dodaj(a, nowe_zadanie):
+    """Dodaje nowy element do listy"""
+
     a.append(nowe_zadanie)
 
 
-def usun(lista):
-    lista.pop(int(input('Podaj zadanie do usuniecia: \n')))
+def usun(lista, elem):
+    """Usuwa z listy element o podanym indexie"""
+
+    lista.pop(elem)
 
 
 def done(lista):
@@ -14,12 +17,16 @@ def done(lista):
 
 
 def drukuj():
+    """Drukuje naglowek i zawartosc pliku ToDo.txt"""
+
     print('-- LISTA ZADAŃ DO ZROBIENIA --')
     with open('ToDo.txt') as plik :
         print(plik.read())
-    
-        
+
+
 def odczyt():
+    """Wczytuje i formatuje zawartosc pliku ToDo.txt do listy i zwraca ta liste"""
+
     file_cont = []
     str_pom = ''
     with open('ToDo.txt') as plik :
@@ -29,9 +36,11 @@ def odczyt():
         str_pom = str_pom[4:-1]
         file_cont[i] = str_pom
     return file_cont
-        
-    
+
+
 def zapis(temp_list):
+    """Formatuje zawartosc listy i zapisuje do pliku ToDo.txt"""
+
     with open('ToDo.txt','w') as plik :
         for p in range(len(temp_list)):
             linijka = '{} - {}\n'.format(p,temp_list[p])
@@ -39,15 +48,19 @@ def zapis(temp_list):
 
 
 def slownik(polecenie):
+    """Odczytuje z klawiatury wpisane polecenie i je wykonuje jesli jest w slowniku"""
+
     if polecenie in ['dodaj','DODAJ','add','ADD']:
-        dodaj(lista)
+        nowe_zadanie = input('Wpisz nowe zadanie: \n')
+        dodaj(lista, nowe_zadanie)
         zapis(lista)
     if polecenie in ['usun','USUN','del','DEL']:
-        usun(lista)
+        elem = int(input('Podaj element do usuniecia: \n'))
+        usun(lista, elem)
         zapis(lista)
     if polecenie in ['done']:
         done(lista)
-    return 
+    return
 
 """------------------"""
 lista = odczyt()
@@ -56,5 +69,3 @@ while polecenie not in ['EXIT','exit']:
     slownik(polecenie)
     drukuj()
     polecenie = input('dodaj, usun, EXIT: ')
-
-
