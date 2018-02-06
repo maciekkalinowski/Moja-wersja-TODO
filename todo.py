3d8d0e7ea4d3bff121cf8b58a22a4928de2fab5b
Maciek Kalinowski <maciek.kalinowski@gmail.com>
2018-02-06 22:36:23 +0100

    Usunąłem wykomentowany kod

--- todo.py
+++ todo.py
@@ -1,52 +1,5 @@
 
 
-"""
-def drukuj():
-    
-
-    print('-- LISTA ZADAŃ DO ZROBIENIA --')
-    with open('ToDo.txt') as plik :
-        print(plik.read())
-
-def odczyt():
-    
-
-    file_cont = []
-    str_pom = ''
-    with open('ToDo.txt') as plik :
-        file_cont = plik.readlines()
-    for i in range(len(file_cont)):
-        str_pom = file_cont[i]
-        str_pom = str_pom[4:-1]
-        file_cont[i] = str_pom
-    return file_cont
-
-def zapis(temp_list):
-    
-
-    with open('ToDo.txt','w') as plik :
-        for p in range(len(temp_list)):
-            linijka = '{} - {}\n'.format(p,temp_list[p])
-            plik.write(linijka)
-
-
-def slownik(polecenie):
-    
-
-    if polecenie in ['dodaj','DODAJ','add','ADD']:
-        nowe_zadanie = input('Wpisz nowe zadanie: \n')
-        dodaj(lista, nowe_zadanie)
-        zapis(lista)
-    if polecenie in ['usun','USUN','del','DEL']:
-        elem = int(input('Podaj element do usuniecia: \n'))
-        usun(lista, elem)
-        zapis(lista)
-    if polecenie in ['done']:
-        done(lista)
-    return
-"""
-
-
 def dodaj(a, nowe_zadanie):
     """Dodaje nowy element do listy"""
 
@@ -117,16 +70,9 @@ def slownik(polecenie):
     return
 
 """------------------"""
-#lista = odczyt()
 lista = odczyt()
 polecenie = ''
 while polecenie not in ['EXIT','exit']:
     slownik(polecenie)
     drukuj()
     polecenie = input('dodaj, usun, EXIT: ')
-"""polecenie = ''
-while polecenie not in ['EXIT','exit']:
-    slownik(polecenie)
-    drukuj()
-    polecenie = input('dodaj, usun, EXIT: ')
-"""
